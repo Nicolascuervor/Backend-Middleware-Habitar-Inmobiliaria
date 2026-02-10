4d0ecc7aded01264eacefc4958dc3dc8b6e65bac
Nicolás <ncuervo_175@cue.edu.co>
2026-02-09 19:07:12 -0500

    Initial commit

---
+++ middleware-service/src/main/java/co/habitarInmobiliaria/middleware_service/MiddlewareServiceHabitarInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package co.habitarInmobiliaria.middleware_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareServiceHabitarInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareServiceHabitarInmobiliariaApplication.class, args);
+	}
+
+}
