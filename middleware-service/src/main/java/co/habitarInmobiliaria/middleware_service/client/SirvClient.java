a732828cd6443b07bf26e6f46522f741b32dc51e
Nicolás <ncuervo_175@cue.edu.co>
2026-03-23 19:32:51 -0500

    implementacion de Card HubSpot

--- middleware-service/src/main/java/co/habitarInmobiliaria/middleware_service/client/SirvClient.java
+++ middleware-service/src/main/java/co/habitarInmobiliaria/middleware_service/client/SirvClient.java
@@ -8,19 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "sirvClient", url = "${sirv.api.base-url}")
 public interface SirvClient {
 
-    // 1. Obtener el Token de Seguridad
-    @PostMapping("/token")
-    SirvTokenResponseDTO obtenerToken(@RequestBody SirvTokenRequestDTO request);
-
-    // 2. Subir el archivo binario
-    @PostMapping(value = "/files/upload", consumes = "*/*")
-    void subirArchivo(
-            @RequestHeader("Authorization") String bearerToken,
-            @RequestHeader("Content-Type") String contentType,
-            @RequestParam("filename") String filename,
-            @RequestBody byte[] fileBytes
-    );
 }
